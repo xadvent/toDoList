eed5ec3be53dfe3b8409c15ed7cff89166f72806
xadvent <piercestrutt@gmail.com>
2023-08-30 22:28:26 -0600

    fixed multiple issues regarding editing tasks
    fix 1: stopped deletion of task when clicking the 'edit' button and
    exiting back to another tab.
    
    fix 2: stopped duplication of tasks when submitting an edited task

--- src/task_buttons/taskItemHoverFunctions.js
+++ src/task_buttons/taskItemHoverFunctions.js
@@ -1,5 +1,7 @@
 import { projectContainer } from "../task_controls/taskManagers";
 import { addNew } from "../content_tabs/form_tab/addNew";
+import { submitFunction } from "../content_tabs/form_tab/addNewFunctions";
+import { overview } from "../content_tabs/sidebar_elements/sidebarTabCreation";
 
 const getTaskByTitle = (current) => {
     const infoTitle = current.parentElement.parentElement.firstChild.textContent;
@@ -44,18 +46,63 @@ export const clickDelete = function () {
     }
 };
 
-export const editTask = function(){
+export const editTask = function () {
     addNew()
     const holder = getTaskByTitle(this)
+    const title = document.querySelector(`input[name='title']`)
+    const description = document.querySelector('textarea')
+    const importance = document.querySelector('select[name="importance"]')
+    const project = document.querySelector('select[id="project"]')
+    const date = document.querySelector('input[type="date"]')
+    const button = document.querySelector('button[type="submit"]')
+
     document.querySelector('.new-form-tab h1').textContent = 'Edit Task'
-    document.querySelector(`input[name='title']`).value = holder.mainTask.title
-    document.querySelector('textarea').textContent = holder.mainTask.description
-    document.querySelector('select[name="importance"]').value = holder.mainTask.importance
-    document.querySelector('select[id="project"]').value = holder.taskProject.name
-    document.querySelector('input[type="date"]').value = holder.mainTask.date
 
-    document.querySelectorAll('.form-info')
+    title.value = holder.mainTask.title
+    description.textContent = holder.mainTask.description
+    importance.value = holder.mainTask.importance
+    project.value = holder.taskProject.name
+    date.value = holder.mainTask.date
 
-    holder.mainTask.removeFromList(projectContainer.allTaskManager.tasklist)
-    holder.projectTask.removeFromList(holder.taskProject.taskManager.tasklist)
+
+    // Function to serialize form data into an object
+    function serializeForm(form) {
+        const formData = new FormData(form);
+        const serialized = {};
+        for (const [key, value] of formData.entries()) {
+            serialized[key] = value;
+        }
+        return serialized;
+    }
+
+    // Store the initial form data when the form is displayed
+    const initialFormData = serializeForm(document.querySelector('#add-form'));
+
+    // Function to check for changes and submit the form
+    const checkAndSubmit = function(event) {
+        event.preventDefault();
+
+        // Serialize the current form data
+        const currentFormData = serializeForm(document.querySelector('#add-form'));
+
+        const isFormChanged = JSON.stringify(initialFormData) !== JSON.stringify(currentFormData);
+
+        if (isFormChanged) {
+            // Update the task properties with the new values
+            holder.mainTask.title = title.value;
+            holder.mainTask.description = description.value;
+            holder.mainTask.importance = importance.value;
+            holder.mainTask.date = date.value;
+            holder.taskProject.name = project.value;
+
+            overview()
+            projectContainer.allTaskManager.refresh();
+        } else {
+            overview()
+        }
+    }
+
+    const submitButton = document.querySelector('button[type="submit"]');
+    button.removeEventListener('click', submitFunction)
+    submitButton.addEventListener('click', checkAndSubmit);
 }
